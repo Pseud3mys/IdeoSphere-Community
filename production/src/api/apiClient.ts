7f4d3bc7b218a73f6ce2ad8ca727e2ce3188a546
Alexandre H <alexandre.hersent@gmail.com>
2025-10-23 16:54:07 +0200

    chemin relatif

--- production/src/api/apiClient.ts
+++ production/src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // L'URL de base de votre API backend.
 // Idéalement, ceci devrait provenir d'une variable d'environnement.
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = '/api';
 
 /**
  * Instance Axios pré-configurée pour toutes les requêtes API.
