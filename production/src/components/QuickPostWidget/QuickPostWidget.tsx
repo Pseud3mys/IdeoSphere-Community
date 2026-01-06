dd65032ba567fa87ce734c6ca212a6174ed5a737
Alexandre H <alexandre.hersent@gmail.com>
2026-01-06 14:06:58 +0100

    ajout bordure widget

--- production/src/components/QuickPostWidget/QuickPostWidget.tsx
+++ production/src/components/QuickPostWidget/QuickPostWidget.tsx
@@ -105,7 +105,7 @@ export function QuickPostWidget({
 
   return (
     <div className={standalone ? 'min-h-screen p-4' : ''}>
-      <div className={standalone ? 'max-w-2xl mx-auto' : ''}>
+      <div className={standalone ? 'max-w-2xl mx-auto bg-white rounded-lg border border-black/20 shadow-sm overflow-hidden p-6' : ''}>
         {currentView === 'composer' ? (
           <QuickPostComposer
             groupIds={defaultGroupIds}
